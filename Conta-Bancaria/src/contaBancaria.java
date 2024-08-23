5e86546987d28a6e0d5a58399e37f89110b95926
Nicolas Eidi <nicolas27@outlook.com>
2024-08-23 08:34:48 -0300

    Conta bancaria

--- Conta-Bancaria/src/contaBancaria.java
+++ Conta-Bancaria/src/contaBancaria.java
@@ -4,9 +4,9 @@ public class contaBancaria {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         
-        System.out.println("Insira o valor de sua renda: ");
+        System.out.println("Insira o valor de seu salario: ");
         double salario = scan.nextDouble();
-        System.out.println("Seu salario e: " + salario);
+        System.out.println("Seu salario: " + salario);
         scan.close();
     }
 }
